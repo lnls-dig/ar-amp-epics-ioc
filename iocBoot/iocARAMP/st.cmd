f2896e40a7956aa746020fed1387eb9eed6315d0
Henrique A. Silva <henrique812@gmail.com>
2017-08-28 17:38:09 +0000

    Initial IOC implementation

---
+++ iocBoot/iocARAMP/st.cmd
@@ -0,0 +1,25 @@
+#!../../bin/linux-x86_64/ARAMP
+
+## You may have to change ARAMP to something else
+## everywhere it appears in this file
+
+< envPaths
+
+cd "${TOP}"
+
+epicsEnvSet("STREAM_PROTOCOL_PATH", "$(TOP)/ARAMPApp/Db")
+
+## Register all support components
+dbLoadDatabase "dbd/ARAMP.dbd"
+ARAMP_registerRecordDeviceDriver pdbbase
+
+drvAsynIPPortConfigure("AMPPORT", "${DEVICE_IP}:4003",0,0,0)
+
+## Load record instances
+dbLoadRecords("${TOP}/ARAMPApp/Db/aramp.db", "P=${P}, R=${R}, PORT=AMPPORT")
+
+cd "${TOP}/iocBoot/${IOC}"
+iocInit
+
+## Start any sequence programs
+#seq sncxxx,"user=rootHost"
