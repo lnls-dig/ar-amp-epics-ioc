bdd68fc91d5da47ce29be0b95ece1d44c528ccfb
Henrique A. Silva <henrique812@gmail.com>
2017-09-06 12:27:43 +0000

    Include autosave feature in code
    As we can't readback any status value from the amplifiers, we're leaving the autosave feature disabled for now. If needed in the future, one just need to uncomment the autosave code

--- iocBoot/iocARAMP/st.cmd
+++ iocBoot/iocARAMP/st.cmd
@@ -18,8 +18,20 @@ drvAsynIPPortConfigure("AMPPORT", "${DEVICE_IP}:${DEVICE_PORT}",0,0,0)
 ## Load record instances
 dbLoadRecords("${TOP}/ARAMPApp/Db/aramp.db", "P=${P}, R=${R}, PORT=AMPPORT")
 
+# Specify save file path
+set_savefile_path("$(TOP)", "autosave")
+set_requestfile_path("iocBoot/iocARAMP")
+
+# Enable/Disable backup files (0->Disable, 1->Enable)
+save_restoreSet_DatedBackupFiles(0)
+
+# Number of copies of .sav files to maintain
+save_restoreSet_NumSeqFiles(0)
+
 cd "${TOP}/iocBoot/${IOC}"
 iocInit
 
 ## Start any sequence programs
-#seq sncxxx,"user=rootHost"
+
+# Start autosave
+#create_monitor_set("auto_settings.req", 30, "P=${P}, R={R}")
